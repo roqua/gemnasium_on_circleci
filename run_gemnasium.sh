21b41981c653a6ae6df997b598c96f202aae59a9
Daan Davidsz <daan@roqua.nl>
2017-03-09 13:09:26 +0100

    Revert "Create a script to run"
    This reverts commit b53bafdffd07e0428a700b293374d936be94a88f.

--- run_gemnasium.sh
+++ run_gemnasium.sh
@@ -5,11 +5,5 @@ if [ -n "${CIRCLE_RUN_GEMNASIUM}" ]; then
     sudo apt-get update;
     sudo apt-get install gemnasium-toolbelt;
     [ -z "$GEMNASIUM_TESTSUITE" ] && export GEMNASIUM_TESTSUITE="bundle exec rspec spec";
-
-    echo "#\!/bin/bash\n" > script/gemnasium_tests.sh
-    echo $GEMNASIUM_TESTSUITE >> script/gemnasium_tests.sh
-    chmod +x script/gemnasium_tests.sh
-    export GEMNASIUM_TESTSUITE="script/gemnasium_tests.sh";
-
     gemnasium --token $GEMNASIUM_TOKEN autoupdate run;
 fi
